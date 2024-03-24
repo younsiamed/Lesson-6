948e170bc149feb43188107093c595176a8f04e3
younsiamed <younsiamed@gmail.com>
2024-03-24 23:00:59 +0300

    Lesson 6

---
+++ Макрос SUB/Макрос SUB/Макрос SUB.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+#define SUB(x, y) ((x) - (y))
+
+int main(int argc, char** argv) {
+    int a = 6;
+    int b = 5;
+    int c = 2;
+    std::cout << SUB(a, b) << std::endl;
+    std::cout << SUB(a, b) * c << std::endl;
+    std::cout << SUB(a, b + c) * c << std::endl;
+    return 0;
+}
---
+++ Препроцессорные директивы/Препроцессорные директивы/Препроцессорные директивы.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+#error "Необходимо определить MODE"
+#endif
+
+#if MODE == 0
+#define TRAINING_MODE
+#elif MODE == 1
+#define BATTLE_MODE
+#endif
+
+#ifdef TRAINING_MODE
+void add(int a, int b) {
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+}
+#endif
+
+#ifdef BATTLE_MODE
+void add(int a, int b) {
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    std::cout << "Введите число 1: ";
+    std::cin >> a;
+    std::cout << "Введите число 2: ";
+    std::cin >> b;
+    std::cout << "Результат сложения: " << (a + b) << std::endl;
+}
+#endif
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+    add(0, 0);
+#else
+    std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif
+
+    return 0;
+}
